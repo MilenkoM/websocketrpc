5eeb74e1fc95919b543a94704a84daabd290c578
tguettler <tguettler@tbz-pariv.de>
2013-12-05 14:11:55 +0100

    - tinyrpc verwendet eine blocking-API. keine Ahnung, wie man das mittels pulsar umsetzen kann. Gebe erstmal auf. Siehe meine Frage: https://groups.google.com/forum/#!topic/python-pulsar/Tn3sYop3ejc

--- websocketrpc/client.py
+++ websocketrpc/client.py
@@ -24,21 +24,27 @@ def start(actor):
     actor.event_loop.call_soon(client.start)
     return actor
 
-class WebSocketClientTransport(ClientTransport, ws.WS):
+class WebSocketClientTransport(ClientTransport): # tinyrpc
     ws = None
 
-    def __init__(self):
-        self.queue = Queue()
-
-    def get(self): # TODO wann wird das aufgerufen?
-        return self.queue.get()
-
-    def on_message(self, ws, message):
-        return self.queue.put(message)
-        self.ws.write(message)
+    def __init__(self, queue):
+        self.queue=queue
 
     def send_message(self, message, expect_reply=True):
+        print 'c tran send_message', message
         self.ws.write(message)
+        deferred=self.queue.get(wait=True)
+        print 'send_message result=%s' % result
+        pulsar.async_while(TIMEOUT, not deferred.done()
+        return result
+
+class WebSocketClient(ws.WS): # pulsar
+    def __init__(self, queue):
+        self.queue=queue
+
+    def on_message(self, ws, message): # receive reply
+        print 'c tran on_message', message
+        return self.queue.put(message)
 
 class Client(object):
     def __init__(self, actor):
@@ -46,9 +52,11 @@ class Client(object):
         self.http=HttpClient()
 
     def start(self):
-        transport = WebSocketClientTransport()
+        queue=Queue()
+        transport=WebSocketClientTransport(queue)
+        wsc = WebSocketClient(queue)
         transport.ws = yield self.http.get('ws://127.0.0.1:8060/jsonrpc',
-                                           websocket_handler=transport).on_headers
+                                           websocket_handler=wsc).on_headers
 
         self.rpc_client=RPCClient(
             JSONRPCProtocol(),
--- websocketrpc/server.py
+++ websocketrpc/server.py
@@ -6,13 +6,82 @@ import pulsar
 from pulsar import Queue
 from pulsar.apps import wsgi, ws
 
-class WebSocketServer(ws.WS):
+from tinyrpc.dispatch import RPCDispatcher
+from tinyrpc.transports import ServerTransport
+from tinyrpc.server import RPCServer
+from tinyrpc.protocols.jsonrpc import JSONRPCProtocol
+
+dispatcher = RPCDispatcher()
+
+@dispatcher.public
+def reverse_string(s):
+    return s[::-1]
+
+class WebSocketServerTransport(ServerTransport): # tinyrpc
+    def __init__(self, websocket):
+        self.websocket=websocket
+
+    def receive_message(self):
+        '''
+        called from rpc_server.server_forever(). Blocks until data available.
+        We don't block, we receive events.
+        returns (context, message)
+        '''
+        raise NotImplementedError()
+    
+    def handle_message(self):
+        '''
+        Copy
+        '''
+
+    def send_reply(self, context, reply):
+        print 'send_reply', repr(reply)
+        self.websocket.write(reply)
+
+class WebSocketRPCServer(RPCServer): # tinyrpc
+    def server_forever(self):
+        # not used
+        while True:
+            self.receive_one_message()
+            
+    def receive_one_message(self):
+        # not used
+        # copied from RPCServer.server_forever(). https://github.com/mbr/tinyrpc/pull/9
+        context, message = self.transport.receive_message()
+        return self.handle_message(context, message)
+    
+    def handle_one_message(self, context, message):
+        # assuming protocol is threadsafe and dispatcher is theadsafe, as
+        # long as its immutable
+
+        def handle_message(context, message):
+            try:
+                request = self.protocol.parse_request(message)
+            except RPCError as e:
+                response = e.error_respond()
+            else:
+                response = self.dispatcher.dispatch(request)
+
+            # send reply
+            self.transport.send_reply(context, response.serialize())
+
+        self._spawn(handle_message, context, message)
+
+class Context(object):
+    pass
+
+class WebSocketServer(ws.WS): # pulsar
     def on_open(self, websocket): # from ws.WS
-        print 'on_open', websocket
-        self.messages=Queue()
+        print 'on_open', websocket, id(websocket)
+        websocket.rpc_server=WebSocketRPCServer(
+            WebSocketServerTransport(websocket),
+            JSONRPCProtocol(),
+            dispatcher
+            )
 
     def on_message(self, websocket, message): # from ws.WS
-        self.messages.put((websocket, message))
+        print 'on_message', websocket, id(websocket), repr(message)
+        websocket.rpc_server.handle_one_message(Context(), message)
         
 class Site(wsgi.LazyWsgi):
 
