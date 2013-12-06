e4f5aab121ba5fe4c5ff415aff72811d53168b89
tguettler <tguettler@tbz-pariv.de>
2013-12-06 11:40:31 +0100

    - tornado chat example is new test_server.py

--- websocketrpc/tests/test_server.py
+++ websocketrpc/tests/test_server.py
@@ -1,33 +1,108 @@
+#!/usr/bin/env python
+# based on tornado chat example
+#
+# Copyright 2009 Facebook
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+"""Simplified chat demo for websockets.
+
+Authentication, error handling, etc are left as an exercise for the reader :)
+"""
+
 import logging
-import unittest
-import argparse
-import subprocess
+import tornado.escape
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import tornado.websocket
+import os.path
+import uuid
 
-TEST_SERVER_PORT=18937
-from websocketrpc import Server, websocket
+from tornado.options import define, options
 
-class MyTestCase(unittest.TestCase):
-    def test_start(self):
-        cmd=['python', __file__, '--port=%s' % TEST_SERVER_PORT]
-        pipe=subprocess.Popen(cmd)
-        try:
-            self._test_start(pipe)
-        finally:
-            pipe.terminate()
+define("port", default=8888, help="run on the given port", type=int)
 
-    def _test_start(self, pipe):
-        pass
 
-class TestServer(Server, websocket.WebSocketHandler): # TODO switch from multiple inheritance to delegation
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler),
+            (r"/chatsocket", ChatSocketHandler),
+        ]
+        settings = dict(
+            cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            xsrf_cookies=True,
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
 
-    def __init__(self, *args, **kwargs):
-        # gets called on first request.
-        Server.__init__(self, **kwargs)
-        websocket.WebSocketHandler.__init__(self, *args)
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html", messages=ChatSocketHandler.cache)
+
+class ChatSocketHandler(tornado.websocket.WebSocketHandler):
+    waiters = set()
+    cache = []
+    cache_size = 200
+
+    def allow_draft76(self):
+        # for iOS 5.0 Safari
+        return True
+
+    def open(self):
+        ChatSocketHandler.waiters.add(self)
+
+    def on_close(self):
+        ChatSocketHandler.waiters.remove(self)
+
+    @classmethod
+    def update_cache(cls, chat):
+        cls.cache.append(chat)
+        if len(cls.cache) > cls.cache_size:
+            cls.cache = cls.cache[-cls.cache_size:]
+
+    @classmethod
+    def send_updates(cls, chat):
+        logging.info("sending message to %d waiters", len(cls.waiters))
+        for waiter in cls.waiters:
+            try:
+                waiter.write_message(chat)
+            except:
+                logging.error("Error sending message", exc_info=True)
+
+    def on_message(self, message):
+        logging.info("got message %r", message)
+        parsed = tornado.escape.json_decode(message)
+        chat = {
+            "id": str(uuid.uuid4()),
+            "body": parsed["body"],
+            }
+        chat["html"] = tornado.escape.to_basestring(
+            self.render_string("message.html", message=chat))
+
+        ChatSocketHandler.update_cache(chat)
+        ChatSocketHandler.send_updates(chat)
+
 
 def main():
-    TestServer.parse_args_and_run()
+    tornado.options.parse_command_line()
+    app = Application()
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     main()
