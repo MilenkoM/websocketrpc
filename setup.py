20434c59c715d491137406903ea64eca0f82b533
Thomas Güttler <tguettler@tbz-pariv.de>
2014-03-24 15:04:58 +0100

    - version in __init__.py. Defined once, not twice.

--- setup.py
+++ setup.py
@@ -1,15 +1,31 @@
 import os
 import setuptools
 
-version = "0.1"
-
-
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+# from https://github.com/pypa/sampleproject/blob/master/setup.py
+# Read the version number from a source file.
+# Why read it, and not import?
+# see https://groups.google.com/d/topic/pypa-dev/0PkjVpcxTzQ/discussion
+def find_version(*file_paths):
+    # Open in Latin-1 so that we avoid encoding errors.
+    # Use codecs.open for Python 2 compatibility
+    with codecs.open(os.path.join(here, *file_paths), 'r', 'latin1') as f:
+        version_file = f.read()
+
+    # The version line must have the form
+    # __version__ = 'ver'
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
 setuptools.setup(
     name='websocketrpc',
-    version=version,
+    version=find_version('websocketrpc', '__init__.py'),
     license="http://www.apache.org/licenses/LICENSE-2.0",
     packages=setuptools.find_packages(),
     long_description=read('README.rst'),
--- websocketrpc/__init__.py
+++ websocketrpc/__init__.py
@@ -1,3 +1,5 @@
+__version__='0.2.0'
+
 from server import Server
 from client import Client
 
